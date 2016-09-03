c5a66bfdc01e22b67b38b6bb34aa3ccc6fdc3607
Steve Klabnik <steve@steveklabnik.com>
2016-09-03 19:07:05 -0400

    Even better Rust.
    :heart: @gankro

--- rarray.rs
+++ rarray.rs
@@ -9,12 +9,12 @@ pub type Value = libc::uintptr_t;
 #[allow(non_upper_case_globals)]
 static Qnil: libc::uintptr_t = 0x08 as libc::uintptr_t;
 
+use core::slice;
+
 #[no_mangle]
 pub unsafe extern fn rb_mem_clear(mem: *mut Value, size: libc::c_long) {
-    for i in 0..size {
-        let mem = mem.offset(i as isize);
-
-        *mem = Qnil;
+    for x in slice::from_raw_parts_mut(mem, size as usize) {
+        *x = Qnil;
     }
 }
 
